e8f075ec2daa713f659266306fb63f79f1877a13
SheaW07 <102532319+SheaW07@users.noreply.github.com>
2022-03-28 20:01:17 -0400

    Update CrossProductDeterminate.m

--- CrossProductDeterminate.m
+++ CrossProductDeterminate.m
@@ -1,34 +1,48 @@
-Fdir = [4, -4, -2]; %Force direction component form
-F = (120 / norm(Fdir)) * Fdir; %Froce magnitude
-x1 = 1; %x component vector 1
-y1 = 4; %y  component vector 1
-z1 = 2; %z component vector 1
-x2 = 5; %x component vector 2
-y2 = 0; %y component vector 2
-z2 = 0; %z component vector 2
-vector1 = [x1, y1, z1] %Vector 1 component form
-vector2 = [x2, y2, z2] %Vector 2 component form
-pBC = vectorC - vectorB %Position vector
-intermediate = pBC.*pBC %Intermidiate for mathematical operation
-den = sum(intermediate) %Creates denominator for following function
-uBC = pBC./sqrt(den) %Unit vector
-crossPrdctMat = [vectorB;F] %Matrix for determining cross product
-for indx = 1:3 %for loop for determining cross product
-    detMat = crossPrdctMat; 
+%Create initial vectors - part 1
+%create and display vector r (vector that points from the origin to the
+%point the force is applied)
+%custom inputs for vector components
+xcomp = 1;
+ycomp = 4; 
+zcomp = 2;
+r = [xcomp, ycomp, zcomp]; %format: [x comp, y comp, z comp]
+disp("r = " + vectorToStr(r));
+%create and display vector F (vector with a magnitude of 120 pounds that
+%points in the direction of Fdir)
+%Custom direction inputs
+xdir = 4;
+ydir = -4;
+zdir = -2;
+Fdir = [xdir, ydir, zdir];
+F = (120 / norm(Fdir)) * Fdir;
+disp("F = " + vectorToStr(F));
+%Adds formating + clears up a now obsolete variable)
+disp(" ");
+clear Fdir;
+
+%Calculate Cross Product - part 2
+crossPrdctMat = [r; F]; %Creates a matrix with one vector matrix on top of the other; used for calculating r x F
+T = zeros(1, 3); %Creates empty 3d vector for storing the Cross Product
+for indx = 1:3
+    detMat = crossPrdctMat;
     detMat(:, indx) = []; %detMat is set to equal the adjuct matrix of the component being solved for
     T(indx) = det(detMat); %determinant of adjunct matrix found and stored as the coresponding component of the cross product vector
 end
-disp("r x F = " + vectoStr(T)); %display vector in string form
 
-function str = vectoStr(v) %function to convert a vector into a string - v2
-   
-    if ((size(v, 1) > 1 && size(v, 2) > 1) || length(size(v)) > 2) %error handler
+%Display Output - part 3 Shea Wills
+disp("r x F = " + vectorToStr(T));
+
+%function to convert a vector into a string - v2
+function str = vectorToStr(v)
+    %Check to make sure inputted matrix is one dimensional (a vector).
+    %Otherwise, throw an error
+    if ((size(v, 1) > 1 && size(v, 2) > 1) || length(size(v)) > 2)
         error("Input Vector, v, Must be a 1 Dimensional Array");
     end
     %start output string with a '<' character
     str = "<";
     for indx = 1:length(v)
-        %add the correlating component value and a ', ' for each value present
+        %add the coresponding number and a ', ' for each value present
         str = str + v(indx) + ", ";
     end
     %end output string with a '>' character
